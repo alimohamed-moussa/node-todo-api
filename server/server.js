0ab2de9fe2d9b19c5c1ded92111cf91b54c83614
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-07-31 00:49:56 +0200

    Add GET/ todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,6 +27,24 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+  // Valid Id?
+  if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById({_id: id}).then((todo) => {
+    if (!todo) {
+     return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
