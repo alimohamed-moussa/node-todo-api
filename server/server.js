b1f45727a73d0798b9eef109114175276b5af632
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-07-31 14:21:22 +0200

    correct issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -24,25 +24,25 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos)=> {
+  Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos/:id', (req, res) =>{
+app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
-  // Valid Id?
+
   if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
+    return res.status(404).send();
   }
 
-  // findById
-  Todo.findById({_id: id}).then((todo) => {
+  Todo.findById(id).then((todo) => {
     if (!todo) {
-     return res.status(404).send();
+      return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) =>{
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
