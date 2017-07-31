24f7f629d82dc18ff7524fe3ba009ca4b5d0a089
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-07-31 21:36:28 +0200

    Fix

--- server/server.js
+++ server/server.js
@@ -73,22 +73,22 @@ app.patch('/todos/:id', (req, res) => {
   var body = _.pick(req.body, ['text', 'completed']);
 
   if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
+    return res.status(404).send();
   }
 
   if (_.isBoolean(body.completed) && body.completed) {
-      body.completedAt = new Date().getTime();
+    body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
-      if (!todo) {
-        return res.status(404).send();
-      }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
-      res.send({todo});
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   })
