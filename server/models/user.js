8099d4587f90d2e6783533888b8e94a83f860457
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-08-03 18:16:25 +0200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -7,8 +7,8 @@ var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    minlength: 1,
     trim: true,
+    minlength: 1,
     unique: true,
     validate: {
       validator: validator.isEmail,
@@ -45,10 +45,29 @@ UserSchema.methods.generateAuthToken = function () {
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
   user.tokens.push({access, token});
-  user.save().then(() => {
+
+  return user.save().then(() => {
     return token;
   });
 };
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -53,22 +54,22 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
-    var id = req.params.id;
+  var id = req.params.id;
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
-      if (!todo) {
-        return res.status(404).send();
-      }
-       res.send({todo});
-    }).catch((e) => {
-      res.status(404).send();
-    });
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
-
 
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
@@ -95,19 +96,25 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
+
 // POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
-  return user.generateAuthToken();
+    return user.generateAuthToken();
   }).then((token) => {
-  res.header('x-auth', token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
